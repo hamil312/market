60143b9cef005ed9fbf3b24ad75f8030b5a66bd1
Hamilton Insandara <hamisan36@gmail.com>
2025-04-03 20:32:59 -0500

    fixed i guess

--- src/main/java/com/example/market/infraestructure/mapper/ClienteMapper.java
+++ src/main/java/com/example/market/infraestructure/mapper/ClienteMapper.java
@@ -3,23 +3,22 @@ package com.example.market.infraestructure.mapper;
 import com.example.market.domain.dto.ClientDTO;
 import com.example.market.infraestructure.entity.Cliente;
 import org.mapstruct.InheritInverseConfiguration;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+import org.mapstruct.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @Mapper(componentModel = "spring")
 public interface ClienteMapper {
+
     @Mappings({
-            @Mapping(source = "id", target = "id"),
-            @Mapping(source = "nombre", target = "name"),
-            @Mapping(source = "correo", target = "email"),
-            @Mapping(source = "direccion", target = "address"),
-            @Mapping(source = "telefono", target ="phoneNumber")
+        @Mapping(source = "id", target = "id"),
+        @Mapping(source = "nombre", target = "name"),
+        @Mapping(source = "correo", target = "email"),
+        @Mapping(source = "direccion", target = "address"),
+        @Mapping(source = "telefono", target ="phoneNumber")
     })
-    ClientDTO toClientDTO(Optional<Cliente> cliente);
+
+    ClientDTO toClientDTO(Cliente cliente);
     List<ClientDTO> toClientsDTO(List<Cliente> clientes);
 
     @InheritInverseConfiguration
